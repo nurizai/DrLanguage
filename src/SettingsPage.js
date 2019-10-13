4e00853979add98439f5244ea43c08f0da988e1a
nurizai <nuri_nb@hotmail.de>
2019-10-13 17:28:49 +0200

    Upload to cloudinary

--- src/SettingsPage.js
+++ src/SettingsPage.js
@@ -1,33 +1,63 @@
 import React from 'react'
 import styled from 'styled-components/macro'
 import Page from './Page'
+import axios from 'axios'
 
 export default function SettingsPage({ onSubmit, title}) {
 
-  function handleSubmit(event) {
+  const CORS_ANYWHERE = 'https://cors-anywhere.herokuapp.com/';
+  const CLOUDNAME = process.env.REACT_APP_CLOUDINARY_CLOUDNAME
+  const PRESET = process.env.REACT_APP_CLOUDINARY_PRESET
+
+  function uploadToCloudinary(event) {
     event.preventDefault()
+    const cloudinaryUrl = `${CORS_ANYWHERE}https://api.cloudinary.com/v1_1/${CLOUDNAME}/upload`
+    const photo = event.target.querySelector('input[name="photo"]').files[0]
     const form = event.target
+
+    const formData = new FormData()
+    formData.append('file', photo)
+    formData.append('upload_preset', PRESET)
+
+    axios
+      .post(cloudinaryUrl, formData, {
+        headers: {
+          'Content-type': 'multipart/form-data',
+        },
+      })
+      .then(res => sendToMongoDB(form, res.data.url))
+      .catch(err => console.log(err))
+  }
+
+  function sendToMongoDB(target, url) {
+    const form = target
     const formData = new FormData(form)
     const data = Object.fromEntries(formData)
     const tags = data.tags.split(" ").map(tag => tag.toLowerCase())
-    const dataWithTagsArray = {
+    const formattedData = {
       ...data,
-      tags: tags
+      tags: tags,
+      photo: url
     }
 
-    onSubmit(dataWithTagsArray)
+    onSubmit(formattedData)
     form.reset()
     form.name.focus()
   }
 
   return (
     <Page title={title}>
-      <FormStyled onSubmit={handleSubmit}>
+      <FormStyled onSubmit={uploadToCloudinary}>
 
         <LabelStyled>
           Name
         </LabelStyled>
         <input name="name" type="text"/>
+
+        <LabelStyled>
+          Foto
+        </LabelStyled>
+        <input name="photo" type="file"/>
 
         <LabelStyled>
           Fachbereich
@@ -75,16 +105,16 @@ const FormStyled = styled.form`
   padding: 20px;
   margin-top: 48px;
 
-> input {
-  box-sizing: border-box;
-  padding: 10px;
-  border: 1px solid lightgrey;
-  border-radius: 5px;
-  cursor: pointer;
+  input {
+    box-sizing: border-box;
+    padding: 10px;
+    border: 1px solid lightgrey;
+    border-radius: 5px;
+    cursor: pointer;
 
-    &:hover {
-      border: 1px solid #4882BB;
-    }
+      &:hover {
+        border: 1px solid #4882BB;
+      }
   }
 `
 

b303fa3ecd2f64e446d2b0c188e108b389f2f1b8
nurizai <nuri_nb@hotmail.de>
2019-10-27 14:32:44 +0100

    Wip

--- src/Map.js
+++ src/Map.js
@@ -1,30 +1,15 @@
-import React, { useState }  from 'react'
-import MapGl from 'react-map-gl'
+import React from 'react'
 import Page from './Page'
 
-const token = process.env.REACT_APP_MAPBOX_TOKEN
 export default function Map({ title, data }) {
 
   console.log(data)
 
-  const [viewport, setViewport] = useState({
-    latitude: 53.551086,
-    longitude: 9.993682,
-    width: "100vw",
-    height: "100vh",
-    zoom: 10
-
-  })
   return (
     <Page title={title}>
-      <MapGl {...viewport}
-      mapboxApiAccessToken={"pk.eyJ1IjoibnVyaTkyIiwiYSI6ImNrMjRsNHIzMzBnbnkzZ3J3MXd3OXB1Z3EifQ.k46K4QsDr5_y2mAlSq8HcA"}
-      mapStyle="mapbox://styles/nuri92/ck25zvu6l05ew1co4hec0mj1i"
-      onViewportChange={viewport => {
-        setViewport(viewport);
-      }}
-      >
-      </MapGl>
+      <br /><br /><br />
+      <h1>Here comes the map yo!</h1>
     </Page>
   )
+
 }

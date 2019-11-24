4b4eb146e7707d11ba3689223c889cee122e5199
Shiraz Khan <shirazahmadkhan27@gmail.com>
2019-11-24 12:12:20 +0100

    Rebuild route

---
+++ server/models/Doctor.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const DoctorSchema = new Schema({
+  name: String,
+  specialist: String,
+  description: String,
+  address: String,
+  tags: Array,
+  photo: String,
+  location: String,
+  isBookmarked: Boolean,
+  email: String,
+  phoneNumber: String
+})
+
+const Doctor = mongoose.model('Doctor', DoctorSchema)
+module.exports = Doctor
---
+++ server/routes/doctors.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+
+// Load Doctors model
+
+
+module.exports = router
